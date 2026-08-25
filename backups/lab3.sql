ee3e65d49a7b20dc9cc1f1b5e3901db6bac726a8
Joliyuh <imyourjulie28@gmail.com>
2026-08-25 19:50:25 +0800

    Added lab3.sql(Books and Borrow)

---
+++ backups/lab3.sql
@@ -0,0 +1,58 @@
+CREATE TABLE books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_title VARCHAR(50) NOT NULL,
+    book_author VARCHAR(100) NOT NULL,
+    book_category VARCHAR(50) NOT NULL,
+    book_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO books (book_title, book_author, book_category) VALUES
+('The Great Gatsby', 'F. Scott Fitzgerald', 'Fiction'),
+('To Kill a Mockingbird', 'Harper Lee', 'Fiction'),
+('1984', 'George Orwell', 'Dystopian'),
+('Pride and Prejudice', 'Jane Austen', 'Romance'),
+('The Catcher in the Rye', 'J.D. Salinger', 'Fiction');
+
+CREATE TABLE borrow(
+    borrow_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT NOT NULL,
+    student_id INT NOT NULL,
+    borrow_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    borrow_return_date TIMESTAMP NULL DEFAULT NULL,
+
+    CONSTRAINT fk_borrow_students FOREIGN KEY (student_id)REFERENCES students(student_id),
+
+    CONSTRAINT fk_borrow_book FOREIGN KEY (book_id)
+    REFERENCES books(book_id)
+);
+
+INSERT INTO borrow (book_id, student_id) VALUES
+(1, 2),
+(2, 1),
+(3, 3);
+
+SELECT 
+    br.borrow_id,
+
+    s.student_id,
+    CONCAT(
+        s.student_first_name,
+        ' ',
+        s.student_last_name
+    ) AS student_name,
+    s.student_course,
+
+    b.book_title,
+    b.book_author,
+    b.book_category,
+
+    br.borrow_date
+
+FROM borrow br
+JOIN students s ON 
+    br.student_id = s.student_id
+JOIN books b ON
+    br.book_id = b.book_id
+
+WHERE br.borrow_return_date IS NULL
+ORDER BY br.borrow_date DESC;
